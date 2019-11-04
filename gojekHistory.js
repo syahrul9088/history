5ef76c4521ccf6e82b217413e56bae45a3406eb9
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2019-11-04 12:58:56 +0700

    Update gojekHistory.js

--- gojekHistory.js
+++ gojekHistory.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
+const uuid = require('uuid/v4');
+const sessionID = uuid();
 const readline = require('readline-sync');
-const uuid = require('uuidv4').default;
-const sessionID = uuid()
 const chalk = require('chalk');
 const delay = require('delay');
 
@@ -17,149 +17,145 @@ const unik = length =>
         resolve(text);
     });
 
-const functionSendOtp = (nomor, sessionID, unikID) => new Promise((resolve, reject) => {
-        const url = 'https://api.gojekapi.com/v4/customers/login_with_phone';
-        const bodys = {
-            phone: `+${nomor}`
-        }
-        fetch(url, {
-            method: 'POST',
-            headers: { 
-                'X-Session-ID': sessionID,
-                'X-Platform': 'Android',
-                'X-UniqueId': unikID,
-                'X-AppVersion': '3.34.1',
-                'X-AppId': 'com.gojek.app',
-                'Accept': 'application/json',
-                'X-PhoneModel': 'Android,Custom',
-                'X-PushTokenType': 'FCM',
-                'X-DeviceOS': 'Android,6.0', 
-                'Authorization': 'Bearer',
-                'Accept-Language': 'en-ID',
-                'X-User-Locale': 'en_ID',
-                'Content-Type': 'application/json; charset=UTF-8',
-                'User-Agent': 'okhttp/3.12.1'
-            },
-            body: JSON.stringify(bodys)
-        })
-        .then(res => res.json())
-        .then(result => {
-            resolve(result)
-        })
-        .catch(err => {
-            reject(err)
-        })
-    
-    });
 
-const functionVerifOtp = (sessionID, unikID, otp, loginToken) => new Promise((resolve, reject) => {
-        const url = 'https://api.gojekapi.com/v4/customers/login/verify';
-        const bodys = {
-            "client_name":"gojek:cons:android",
-            "client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e",
-            "data":
-            {
-                "otp": otp,
-                "otp_token": loginToken
-            },
-            "grant_type":"otp",
-            "scopes":"gojek:customer:transaction gojek:customer:readonly"
-        };
-        fetch(url, {
-            method: 'POST',
-            headers: { 
-                'X-Session-ID': sessionID,
-                'X-Platform': 'Android',
-                'X-UniqueId': unikID,
-                'X-AppVersion': '3.34.1',
-                'X-AppId': 'com.gojek.app',
-                'Accept': 'application/json',
-                'X-PhoneModel': 'Android,Custom',
-                'X-PushTokenType': 'FCM',
-                'X-DeviceOS': 'Android,6.0', 
-                'Authorization': 'Bearer',
-                'Accept-Language': 'en-ID',
-                'X-User-Locale': 'en_ID',
-                'Content-Type': 'application/json; charset=UTF-8',
-                'User-Agent': 'okhttp/3.12.1'
-            },
-            body: JSON.stringify(bodys)
-        })
-        .then(res => res.json())
-        .then(result => {
-            resolve(result)
-        })
-        .catch(err => {
-            reject(err)
-        })
-    
-    });
+const functionLogin = (nomor, unikID, sessionID) => new Promise((resolve, reject) => {
+	const url = 'https://api.gojekapi.com/v4/customers/login_with_phone'
+
+	const bodys = {
+        "phone": nomor
+    }
+
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'X-Session-ID': unikID,
+			'X-UniqueId': sessionID,
+			'X-AppVersion': '3.34.1',
+			Authorization: 'Bearer',
+			'Content-Type': 'application/json; charset=UTF-8'
+		},
+		body: JSON.stringify(bodys)
+	})
+	.then(res => res.json())
+	.then(result => {
+		resolve(result)
+	})
+	.catch(err => {
+		reject(err)
+	})
+});
+
+const functionVerify = (sessionID, unikID, otp, otpToken) => new Promise((resolve, reject) => {
+    const url = 'https://api.gojekapi.com/v4/customers/login/verify';
+
+    const bodys = {
+        "client_name":"gojek:cons:android",
+        "client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e",
+        "data":
+        {
+            "otp": otp,
+            "otp_token": otpToken
+        },
+        "grant_type":"otp",
+		"scopes":"gojek:customer:transaction gojek:customer:readonly"
+    };
+
+    fetch (url, {
+        method : 'POST',
+        headers : {
+            'X-Session-ID': sessionID,
+            'X-Platform': 'Android',
+            'X-UniqueId': unikID,
+            'X-AppVersion': '3.34.1',
+            'X-AppId': 'com.gojek.app',
+            'Accept': 'application/json',
+            'X-PhoneModel': 'Android,Custom',
+            'X-PushTokenType': 'FCM',
+            'X-DeviceOS': 'Android,6.0', 
+            'Authorization': 'Bearer',
+            'Accept-Language': 'en-ID',
+            'X-User-Locale': 'en_ID',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'User-Agent': 'okhttp/3.12.1'
+        },
+        body: JSON.stringify(bodys)
+    })
+    .then(res => res.json())
+    .then(result => {
+        resolve(result)
+    })
+    .catch(err => {
+        reject(err)
+    })
+});
 
 const functionCheck = (sessionID, unikID, accessToken) => new Promise((resolve, reject) => {
-        const url = 'https://api.gojekapi.com/wallet/history?page=1&limit=20';
-        fetch(url, {
-            method: 'GET',
-            headers: { 
-                'X-Session-ID': sessionID,
-                'X-Platform': 'Android',
-                'X-UniqueId': unikID,
-                'X-AppVersion': '3.34.1',
-                'X-AppId': 'com.gojek.app',
-                'Accept': 'application/json',
-                'X-PhoneModel': 'Android,Custom',
-                'X-PushTokenType': 'FCM',
-                'X-DeviceOS': 'Android,6.0',
-                'Authorization': `Bearer ${accessToken}`,
-                'Accept-Language': 'en-ID',
-                'X-User-Locale': 'en_ID',
-                'Content-Type': 'application/json; charset=UTF-8',
-                'User-Agent': 'okhttp/3.12.1'
-            }
-        })
-        .then(res => res.json())
-        .then(result => {
-            resolve(result)
-        })
-        .catch(err => {
-            reject(err)
-        })
-    
-    });
+    const url = 'https://api.gojekapi.com/wallet/history?page=1&limit=20';
+    fetch(url, {
+        method: 'GET',
+        headers: { 
+            'X-Session-ID': sessionID,
+            'X-Platform': 'Android',
+            'X-UniqueId': unikID,
+            'X-AppVersion': '3.34.1',
+            'X-AppId': 'com.gojek.app',
+            'Accept': 'application/json',
+            'X-PhoneModel': 'Android,Custom',
+            'X-PushTokenType': 'FCM',
+            'X-DeviceOS': 'Android,6.0',
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept-Language': 'en-ID',
+            'X-User-Locale': 'en_ID',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'User-Agent': 'okhttp/3.12.1'
+        }
+    })
+    .then(res => res.json())
+    .then(result => {
+        resolve(result)
+    })
+    .catch(err => {
+        reject(err)
+    })
+
+});
 
 (async () => {
+    for (var j = 0; j < 1; j++) {
     try{
-    for (var i = 0; i < 20; i++) {
-    const unikID = unik(16)
-    const nomor = readline.question(chalk.yellow('[-] Nomor HP (ex: 62XXX): '))
-    const send = await functionSendOtp(nomor, sessionID, unikID)
-    if (send.success == true) {
+    const unikID = unik(16);
+    const nomor = readline.question(chalk.yellow('[-] Nomor (ex: +1XXXXX): '))
+    const login = await functionLogin(nomor, unikID, sessionID)
+    const otpMsg = login.success
+    if (otpMsg == true) {
         console.log(chalk.green('[+] OTP berhasil dikirim!'))
-    } else if (send.success == false) {
-        const msg = send.errors[0].message
+    } else if (login.success == false) {
+        const msg = login.errors[0].message
         console.log(chalk.red(`[-] ${msg}`))
         break
     }
-    const loginToken = send.data.login_token
+    const otpToken = login.data.login_token
     const otp = readline.question(chalk.yellow('[-] OTP: '))
-    const verif = await functionVerifOtp(sessionID, unikID, otp, loginToken)
-    console.log(verif)
-    const accessToken = verif.data.access_token
-    if (verif.success == true) {
-        console.log(chalk.green('[+] Login berhasil!\n'))
-    }else {
+    const verify = await functionVerify(sessionID, unikID, otp, otpToken)
+    const accessToken = await verify.data.access_token
+    if (verify.success == true) {
+        console.log(chalk.green('[+] Login sukses...\n'))
+    } else {
         console.log(chalk.red('[-] OTP salah!'))
         break
     }
     const check = await functionCheck(sessionID, unikID, accessToken)
+    for (var i = 0; i < 20; i++) {
     const data = check.data.success[i]
     if (data == undefined) {
         break
     }
     await delay (3000)
-    const status = chalk.green(`=> Status: ${data.status}\n=> Jumlah Transaksi: ${data.amount}\n=> Waktu Transaksi: ${data.transacted_at}\n'=> Transaksi Reff: ${data.transaction_ref}\n=> Deskripsi: ${data.description}\n`)
+    const status = chalk.green(`=> Status: ${data.status}\n=> Jumlah Transaksi: ${data.amount}\n=> Waktu Transaksi: ${data.transacted_at}\n=> Transaksi Reff: ${data.transaction_ref}\n=> Deskripsi: ${data.description}\n`)
     console.log(status)
 }
     }catch(e){
-        console.log(e)
+        console.log(e) 
     }
+}
 })();
